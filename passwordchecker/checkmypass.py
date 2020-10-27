abd714a49a38199fc5642185ea8fca3be8979569
Jeremy Hall <jeremyhall@jeremys-mbp.attlocal.net>
2020-10-27 15:30:41 -0400

    adding initial code

--- passwordchecker/checkmypass.py
+++ passwordchecker/checkmypass.py
@@ -1,5 +1,13 @@
 import requests
+import hashlib
 
-url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'
+def requests_api_data(query_char):
+	url = 'https://api.pwnedpasswords.com/range/' + query_char
 res = requests.get(url)
-print(res)
+if res.status_code != 200:
+	raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+return res
+
+def pwned_api_check(password):
+	sha1password = hashlib.sha1(password.encode('utf-8')).hexidigest().upper()
+	return sha1password
