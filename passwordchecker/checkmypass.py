52fb907943d2738a4b896e0c7402312ba423f9bc
Jeremy Hall <jeremyahall1@gmail.com>
2020-10-28 12:12:08 -0400

    Update checkmypass.py
    Punched up the error message

--- passwordchecker/checkmypass.py
+++ passwordchecker/checkmypass.py
@@ -6,7 +6,7 @@ def requests_api_data(query_char):
 	url = 'https://api.pwnedpasswords.com/range/' + query_char
 	res = requests.get(url)
 	if res.status_code != 200:
-		raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+		raise RuntimeError(f'Error fetching: {res.status_code}. Are you determined to fetch errors?')
 	return res
 
 def get_password_leaks_count(hashes, hash_to_check):
